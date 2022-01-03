fb4a1310aad46cdc2c2d65049202544d236a1d4c
shibata0318 <yt031821@gmail.com>
2022-01-03 09:24:21 +0900

    ・前回のコミット漏れ

--- src/resources/views/admin/owners/index.blade.php
+++ src/resources/views/admin/owners/index.blade.php
@@ -14,7 +14,7 @@
                   <div class="container md:px-5 mx-auto">
                     <x-flash-message status="session('status')" />
                     <div class="flex justify-end mb-4"> 
-                      <button onclick="location.href='{{ route('admin.owners.create')}}'" class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">新規登録する</button>                        
+                      <button onclick="location.href=`{{ route('admin.owners.create')}}`" class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">新規登録する</button>                        
                     </div>
                     <div class="lg:w-2/3 w-full mx-auto overflow-auto">
                       <table class="table-auto w-full text-left whitespace-no-wrap">
@@ -32,7 +32,7 @@
                             <td class="md:px-4 py-3">{{ $owner->email }}</td>
                             <td class="md:px-4 py-3">{{ $owner->created_at->diffForHumans() }}</td>
                             <td class="md:px-4 py-3">
-                              <button onclick="location.href='{{ route('admin.owners.edit', ['owner' => $owner->id ])}}'" class="text-white bg-indigo-400 border-0 py-2 px-4 focus:outline-none hover:bg-indigo-500 rounded ">編集</button>                        
+                              <button onclick="location.href=`{{ route('admin.owners.edit', ['owner' => $owner->id ])}}`" class="text-white bg-indigo-400 border-0 py-2 px-4 focus:outline-none hover:bg-indigo-500 rounded ">編集</button>                        
                             </td>
                             <form id="delete_{{$owner->id}}" method="post" action="{{ route('admin.owners.destroy', ['owner' => $owner->id ] )}}">
                               @csrf
